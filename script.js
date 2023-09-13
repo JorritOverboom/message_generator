7fd5f8dd5995fdbada56d15d17b284b8fb2e2159
Jorr1t <jrrtvrbm@gmail.com>
2023-09-13 16:45:32 +0200

    add random selector

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const destination = ['Japan', 'Germany', 'Australia', 'Italy', 'Botswana'];
 const transport = ['swimming', 'walking', 'cycling', 'flying', 'driving'];
 const time = ['one hour', 'two hours', 'three hours', 'four hours', 'five hours'];
 
+const chooseRandom = arr => {
+    let choice = arr[Math.floor(Math.random() * arr.length)]
+    return choice;
+}
