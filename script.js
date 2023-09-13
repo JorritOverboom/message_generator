45649c335336f8fa649bb5a6646a5526422f9079
Jorr1t <jrrtvrbm@gmail.com>
2023-09-13 16:36:35 +0200

    add arrays

---
+++ script.js
@@ -0,0 +1,4 @@
+const destination = ['Japan', 'Germany', 'Australia', 'Italy', 'Botswana'];
+const transport = ['swimming', 'walking', 'cycling', 'flying', 'driving'];
+const time = ['one hour', 'two hours', 'three hours', 'four hours', 'five hours'];
+
